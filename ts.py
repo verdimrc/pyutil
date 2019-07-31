5ef6537758dc1d703d5409029da5d444c507c1cd
Verdi March <verdi.march@gmail.com>
2019-07-31 13:50:30 +0800

    Update ts.py

--- ts.py
+++ ts.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from random import randrange
 
 def gen_ts():
-  """Usage: print(f'jobname-{utc_ts}-{salt}')"""
+  """Usage: print(f'jobname-{utc_ts}utc-{salt}')"""
   utc_ts = datetime.utcnow().strftime('%Y%m%d-%H%M%S')
   salt = randrange(0x7fffffff)
   return utc_ts, salt
