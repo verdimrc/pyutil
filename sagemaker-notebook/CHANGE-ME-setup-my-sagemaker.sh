88c3b3a6574d45716ee3eb9e953b7927d616b1c1
Verdi March <marcverd@amazon.com>
2020-06-17 02:15:48 +0000

    SageMaker notebook: change default shell to bash

--- sagemaker-notebook/CHANGE-ME-setup-my-sagemaker.sh
+++ sagemaker-notebook/CHANGE-ME-setup-my-sagemaker.sh
@@ -5,6 +5,7 @@ echo This is a sample. You should modify this script and run.
 ./adjust-sm-git.sh 'Firstname Lastname' first.last@email.com
 ./change-fontsize.sh
 ./patch-bash-profile.sh
+./set-bash-shell.sh
 
 # Recreate jupyter kernel for each of these custom environments.
 ./reinstall-ipykernel.sh ~/SageMaker/gluonts_p37
