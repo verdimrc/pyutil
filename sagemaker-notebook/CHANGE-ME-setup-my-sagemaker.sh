d60d3e818f5db3657333aaadd12f985baa45af3f
Verdi March <marcverd@amazon.com>
2020-07-13 10:56:50 +0800

    sagemaker notebook: yum install htop & tree

--- sagemaker-notebook/CHANGE-ME-setup-my-sagemaker.sh
+++ sagemaker-notebook/CHANGE-ME-setup-my-sagemaker.sh
@@ -2,6 +2,8 @@
 
 echo This is a sample. You should modify this script and run.
 
+sudo yum install -y htop tree
+
 ./adjust-sm-git.sh 'Firstname Lastname' first.last@email.com
 ./change-fontsize.sh
 ./patch-bash-profile.sh
