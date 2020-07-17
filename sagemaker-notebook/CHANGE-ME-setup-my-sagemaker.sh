8cedfb5db88586f197be5a3db746333c6c12064d
Verdi March <marcverd@amazon.com>
2020-07-17 11:01:24 +0800

    sagemaker-notebook: recolor ipython o.__class__; improve main script

--- sagemaker-notebook/CHANGE-ME-setup-my-sagemaker.sh
+++ sagemaker-notebook/CHANGE-ME-setup-my-sagemaker.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-echo This is a sample. You should modify this script and run.
+# https://gist.github.com/verdimrc/a10dd3ea00a34b0ffb3e8ee8d5cde8b5#file-bash-sh-L20-L34 
+#
+# Utility function to get script's directory (deal with Mac OSX quirkiness).
+# This function is ambidextrous as it works on both Linux and OSX.
+get_bin_dir() {
+    local READLINK=readlink
+    if [[ $(uname) == 'Darwin' ]]; then
+        READLINK=greadlink
+        if [ $(which greadlink) == '' ]; then
+            echo '[ERROR] Mac OSX requires greadlink. Install with "brew install greadlink"' >&2
+            exit 1
+        fi
+    fi
+
+    local BIN_DIR=$(dirname "$($READLINK -f ${BASH_SOURCE[0]})")
+    echo -n ${BIN_DIR}
+}
+
+BIN_DIR=$(get_bin_dir)
+
 sudo yum install -y htop tree
-./adjust-sm-git.sh 'Firstname Lastname' first.last@email.com
-./change-fontsize.sh
-./patch-bash-profile.sh
-./patch-jupyter-config.sh
+${BIN_DIR}/adjust-sm-git.sh 'Verdi March' marcverd@amazon.com
+${BIN_DIR}/change-fontsize.sh
+${BIN_DIR}/patch-bash-profile.sh
+${BIN_DIR}/patch-ipython-config.sh
+${BIN_DIR}/patch-jupyter-config.sh
