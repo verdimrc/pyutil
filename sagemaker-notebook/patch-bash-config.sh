b66c312a0db4e1b4903428719de2e3491ad79e49
Verdi March <you@example.com>
2020-07-19 01:26:03 +0800

    sagemaker-notebook: bash PS1 to always show CONDA_PROMPT_MODIFIER

--- sagemaker-notebook/patch-bash-config.sh
+++ sagemaker-notebook/patch-bash-config.sh
@@ -9,18 +9,37 @@ if [[ ! -z "\$TMUX" ]]; then
         conda deactivate
     done
 fi
+EOF
 
+
+# PS1 must preceed conda bash.hook, to correctly display CONDA_PROMPT_MODIFIER
+cp /home/ec2-user/.bashrc{,.ori}
+cat << EOF > /home/ec2-user/.bashrc
 git_branch() {
-   local branch=\$(git branch 2>/dev/null | grep '^*' | colrm 1 2)
+   local branch=\$(/usr/bin/git branch 2>/dev/null | grep '^*' | colrm 1 2)
    [[ "\$branch" == "" ]] && echo "" || echo "(\$branch) "
 }
+export -f git_branch
 
 # All colors are bold
 COLOR_GREEN="\[\033[1;32m\]"
 COLOR_PURPLE="\[\033[1;35m\]"
 COLOR_YELLOW="\[\033[1;33m\]"
 COLOR_OFF="\[\033[0m\]"
+
+# Define PS1 before conda bash.hook, to correctly display CONDA_PROMPT_MODIFIER
 export PS1="[\$COLOR_GREEN\w\$COLOR_OFF] \$COLOR_PURPLE\\\$(git_branch)\$COLOR_OFF\\\$ "
+
+EOF
+
+
+# Original .bashrc content
+cat /home/ec2-user/.bashrc.ori >> /home/ec2-user/.bashrc
+
+# Custom aliases
+cat << EOF >> /home/ec2-user/.bashrc
+
+alias ll='ls -alF --color=auto'
 EOF
 
 echo "On a new SageMaker terminal, which uses 'sh' by default, type 'bash -l' (without the quotes)"
