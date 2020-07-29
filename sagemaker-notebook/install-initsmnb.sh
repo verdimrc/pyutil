f09f54e18c4462ce1f4b74ad88c745880c13824f
Verdi March <marcverd@amazon.com>
2020-07-29 18:09:35 +0800

    initsmnb: [ongoing] optionally setup EFS 1+ mount(s)

--- sagemaker-notebook/install-initsmnb.sh
+++ sagemaker-notebook/install-initsmnb.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-INITSMNB_DIR=/home/ec2-user/SageMaker/initsmnb
+################################################################################
+# Global vars
+################################################################################
+#INITSMNB_DIR=/home/ec2-user/SageMaker/initsmnb
+INITSMNB_DIR=/tmp/ec2-user/SageMaker/initsmnb
 SRC_PREFIX=https://raw.githubusercontent.com/verdimrc/pyutil/master/sagemaker-notebook
 
 declare -a SCRIPTS=(
@@ -15,6 +19,71 @@ declare -a SCRIPTS=(
     patch-jupyter-config.sh
 )
 
+GIT_USER=''
+GIT_EMAIL=''
+declare -a EFS=()
+
+declare -a HELP=(
+    "[-h|--help]"
+    "[--git-user 'First Last']"
+    "[--git-email me@abc.def]"
+    "[--efs 'fsid,fsap,mp' [--efs ...]]"
+)
+
+################################################################################
+# Helper functions
+################################################################################
+error_and_exit() {
+    echo "$@" >&2
+    exit 1
+}
+
+parse_args() {
+    local key
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+        -h|--help)
+            echo "Install initsmnb."
+            echo "Usage: $(basename ${BASH_SOURCE[0]}) ${HELP[@]}"
+            exit 0
+            ;;
+        --git-user)
+            GIT_USER="$2"
+            shift 2
+            ;;
+        --git-email)
+            GIT_EMAIL="$2"
+            shift 2
+            ;;
+        --efs)
+            [[ "$2" != "" ]] && EFS+=("$2")
+            shift 2
+            ;;
+        *)
+            error_and_exit "Unknown argument: $key"
+            ;;
+        esac
+    done
+}
+
+efs2str() {
+    if [[ ${#EFS[@]} -gt 0 ]]; then
+        printf "'%s'|" "${EFS[@]}"
+    else
+        echo "''"
+    fi
+}
+
+
+################################################################################
+# Main
+################################################################################
+parse_args "$@"
+echo "GIT_USER='$GIT_USER'"
+echo "GIT_EMAIL='$GIT_EMAIL'"
+echo "EFS=$(efs2str)"
+
 mkdir -p $INITSMNB_DIR
 cd $INITSMNB_DIR
 
@@ -25,8 +94,9 @@ curl -fsLO $SRC_PREFIX/{$(echo "${SCRIPTS[@]}" | tr ' ' ',')}
 chmod ugo+x ${SCRIPTS[@]}
 
 echo "Generating setup-my-sagemaker.sh"
-echo "=> git-user / git-email = '$1' / '$2'"
+echo "=> git-user / git-email = '$GIT_USER' / '$GIT_EMAIL'"
 echo "=> fsid / fsap-id / mountpoint = '$3' / '$4' / '$5'"
+echo "=> EFS: (fsid,fsap,mountpoint)|... = $(efs2str)"
 cat << EOF > setup-my-sagemaker.sh
 #!/bin/bash
 
@@ -34,15 +104,20 @@ cat << EOF > setup-my-sagemaker.sh
 
 EOF
 
+# TODO:
+# for i in efs:
+#     echo mount-efs-accesspoint.sh something-something >> setup-my-sagemaker.sh
+#
+# TODO: update mount-efs-accesspoint.sh to deal with multiple efs
 sed \
-    -e "s/Firstname Lastname/$1/" \
-    -e "s/first.last@email.abc/$2/" \
+    -e "s/Firstname Lastname/$GIT_USER/" \
+    -e "s/first.last@email.abc/$GIT_EMAIL/" \
     -e "s/fsid/$3/" \
     -e "s/fsapid/$4/" \
     -e "s/mountpoint/$5/" \
     CHANGE-ME-setup-my-sagemaker.sh >> setup-my-sagemaker.sh
 chmod ugo+x setup-my-sagemaker.sh
-
+[[ "${#EFS[@]}" < 1 ]] && gsed -i "/mount-efs-accesspoint.sh/d" setup-my-sagemaker.sh
 
 EPILOGUE=$(cat << EOF
 
@@ -51,7 +126,7 @@ EPILOGUE=$(cat << EOF
 #                                                         #
 # To change this session, run:                            #
 #                                                         #
-# /home/ec2-user/SageMaker/initsmnb/setup-my-sagemaker.sh #
+# ${INITSMNB_DIR}/setup-my-sagemaker.sh #
 #                                                         #
 # On notebook restart, also run that same command.        #
 ###########################################################
