0d7fa789ff409b02b02b2ec53854191bb1a51e49
Verdi March <verdimrc@users.noreply.github.com>
2022-11-11 18:09:45 +0800

    Enable color for man command

--- initdlami/patch-bash-config.sh
+++ initdlami/patch-bash-config.sh
@@ -44,6 +44,18 @@ prompt_prefix() {
 
 # Define PS1 before conda bash.hook, to correctly display CONDA_PROMPT_MODIFIER
 export PS1="\$(prompt_prefix)[$COLOR_GREEN\w$COLOR_OFF] $COLOR_PURPLE\$(git_branch)$COLOR_OFF\$ "
+
+man() {
+	env \
+		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+		LESS_TERMCAP_md=$(printf "\e[1;31m") \
+		LESS_TERMCAP_me=$(printf "\e[0m") \
+		LESS_TERMCAP_se=$(printf "\e[0m") \
+		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+		LESS_TERMCAP_ue=$(printf "\e[0m") \
+		LESS_TERMCAP_us=$(printf "\e[1;32m") \
+		man "$@"
+}
 EOF
 
 
