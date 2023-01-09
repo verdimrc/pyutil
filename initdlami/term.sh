db2dda8b7d4aa1803e909b526ece2a8cac265352
Verdi March <verdimrc@users.noreply.github.com>
2023-01-09 05:56:30 +0000

    Simplify the bootstrap of kitty.terminfo

--- initdlami/term.sh
+++ initdlami/term.sh
@@ -3,6 +3,10 @@
 # Recognize kitty
 sudo sed -i 's/\(^TERM st-256color$\)/\1\nTERM xterm-kitty/' /etc/DIR_COLORS.256color
 
+# Pre-emptively get kitty's termcap, to avoid the need to 'kitty +kitten ssh <...>'.
+mkdir -p ~/.terminfo
+curl -L https://raw.githubusercontent.com/kovidgoyal/kitty/master/terminfo/kitty.terminfo > ~/.terminfo/kitty.terminfo
+
 # Tone-down directory colors to lighter blue
 TAB=$(printf '\t')
 sudo sed -i "s/^DIR 38;5;27\(${TAB}\)/DIR 38;5;39\1/" /etc/DIR_COLORS.256color
