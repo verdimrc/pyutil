214c4d6b3aa52096118e5560c701e7849ff95bd8
Verdi March <verdimrc@users.noreply.github.com>
2023-03-31 19:13:50 +0800

    Update update.sh

--- initubuntu/update.sh
+++ initubuntu/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo yum update -y
+sudo apt update -y
 
 CONDA=~/.pyenv/versions/miniforge3/bin/conda
 $CONDA update --yes --update-all -n base
@@ -8,13 +8,12 @@ $CONDA update --yes --update-all -n base-p310
 $CONDA clean -a -y
 
 ~/.local/bin/pipx upgrade-all
-~/.local/pipx/venvs/pipupgrade/bin/python3 -m pip install --upgrade \
-    --no-cache-dir 'git+https://github.com/achillesrasquinha/bpyutils.git@develop#egg=bpyutils'
+# ~/.local/pipx/venvs/pipupgrade/bin/python3 -m pip install --upgrade \
+#     --no-cache-dir 'git+https://github.com/achillesrasquinha/bpyutils.git@develop#egg=bpyutils'
 
 # TODO: nvm, cdk
 echo TODO: nvm, cdk
 
-~/initdlami/bat.sh   # TODO: remove old version
 ~/initdlami/delta.sh # TODO: remove old version
 ~/initdlami/s5cmd.sh # TODO: remove old version
 
