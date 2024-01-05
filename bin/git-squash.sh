9c07b934eab14059d6655fa8fe7eb83504ee4c96
Verdi March <verdimrc@users.noreply.github.com>
2024-01-05 06:16:25 +0000

    Add bin/git-squash.sh

---
+++ bin/git-squash.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -euo pipefail
+
+declare -a HELP=(
+    "[-h|--help]"
+    "[-d|--dry-run]"
+    "<COUNT for HEAD~count>"
+)
+
+COUNT=""
+DRY_RUN=0
+
+parse_args() {
+    local key
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case $key in
+        -h|--help)
+            echo "Squash and set timestamp to latest."
+            echo "Usage: $(basename ${BASH_SOURCE[0]}) ${HELP[@]}"
+            exit 0
+            ;;
+        -d|--dry-run)
+            DRY_RUN=1
+            shift
+            ;;
+        *)
+            [[ $COUNT != "" ]] && { shift ; continue ; }
+            COUNT="$1"
+            shift
+            ;;
+        esac
+    done
+
+    [[ $COUNT != "" ]] || { echo "Missing COUNT for HEAD~count" ; exit -1 ; }
+}
+
+parse_args "$@"
+
+TIMESTAMP=$(git show -s --pretty=tformat:%ai HEAD)
+
+echo "
+TIMESTAMP=\$(git show -s --pretty=tformat:%ai HEAD)
+git rebase -i "HEAD~${COUNT}"
+git commit --amend --no-edit --date=\"${TIMESTAMP}\"
+git show --compact-summary HEAD
+
+With vim editor, exit with :cq to inform this script that squash is aborted.
+"
+
+[[ $DRY_RUN == 1 ]] && exit 0
+
+git rebase -i "HEAD~${COUNT}"
+git commit --amend --no-edit --date="${TIMESTAMP}" && RETVAL=$? || RETVAL=$?
+# NOTE: vim :cq will force abort this script
+
+git show --compact-summary HEAD
