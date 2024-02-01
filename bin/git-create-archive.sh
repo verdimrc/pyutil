67656589d93db494217e7fbdf3d3e8d1ceaedd8d
Verdi March <verdimrc@users.noreply.github.com>
2024-02-01 12:03:24 +0800

    Create git-create-archive.sh

---
+++ bin/git-create-archive.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+REPO_DIR=$1
+
+(
+    THIS_DIR=$(pwd)
+    cd $REPO_DIR ;
+    pwd ;
+    PREFIX=$(basename $REPO_DIR)
+    echo git archive --prefix=${PREFIX}/ -o ${THIS_DIR}/${PREFIX}-$(git rev-parse --short HEAD).zip HEAD ;
+)
