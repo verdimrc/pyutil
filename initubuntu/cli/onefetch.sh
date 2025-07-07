85033d1e49a899544661e85c9096fe68b9afe5d7
Verdi March <verdimrc@users.noreply.github.com>
2025-07-07 15:11:34 +0800

    initubuntu: update cli installer script

--- initubuntu/cli/onefetch.sh
+++ initubuntu/cli/onefetch.sh
@@ -2,21 +2,7 @@
 
 set -u
 
-# Constants
-GH=o2sh/onefetch
-TGZ=onefetch-linux.tar.gz
-
-latest_download_url() {
-  curl --silent "https://api.github.com/repos/${GH}/releases/latest" |  # Get latest release from GitHub api
-    grep "\"browser_download_url\": \"https.*\/${TGZ}"               |  # Get download url
-    sed -E 's/.*"([^"]+)".*/\1/'                                        # Pluck JSON value
-}
-
-DOWNLOAD_URL=$(latest_download_url)  # https://github.com/.../download/2.22.0/onefetch-linux.tar.gz
-
-VERSION_LATEST=${DOWNLOAD_URL##*/download/} ; VERSION_LATEST=${VERSION_LATEST%%/*}  # 2.22.0
-VERSION_INSTALLED=$(onefetch --version | cut -d' ' -f2 2> /dev/null)
-[[ ${VERSION_LATEST} == ${VERSION_INSTALLED} ]] && exit 0
-
-curl -Lo /tmp/${TGZ} ${DOWNLOAD_URL}
-sudo tar -xzf /tmp/${TGZ} --no-same-owner -C /usr/local/bin/ ./onefetch
+# https://github.com/o2sh/onefetch/wiki/Installation#ubuntu-ppa
+sudo add-apt-repository ppa:o2sh/onefetch
+sudo apt-get update
+sudo apt-get install onefetch
